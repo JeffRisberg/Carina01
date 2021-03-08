43b0dceaf7a39bb7d38cfe26a62824dc7368aff6
JeffRisberg <j.s.risberg@gmail.com>
2021-03-07 23:09:32 -0800

    pruned content

--- src/main/java/com/incra/carina/demo/gui/pages/HomePage.java
+++ src/main/java/com/incra/carina/demo/gui/pages/HomePage.java
@@ -1,50 +1,49 @@
 package com.incra.carina.demo.gui.pages;
 
-import java.lang.invoke.MethodHandles;
-import java.util.List;
-
+import com.incra.carina.demo.gui.components.FooterMenu;
+import com.incra.carina.demo.gui.components.WeValuePrivacyAd;
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
-import com.qaprosoft.carina.core.gui.AbstractPage;
-import com.incra.carina.demo.gui.components.FooterMenu;
-import com.incra.carina.demo.gui.components.WeValuePrivacyAd;
-
+import java.lang.invoke.MethodHandles;
+import java.util.List;
 
 public class HomePage extends AbstractPage {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    @FindBy(id = "footmenu")
-    private FooterMenu footerMenu;
+  @FindBy(id = "footmenu")
+  private FooterMenu footerMenu;
 
-    @FindBy(xpath = "//div[contains(@class, 'brandmenu-v2')]//a")
-    private List<ExtendedWebElement> brandLinks;
+  @FindBy(xpath = "//div[contains(@class, 'brandmenu-v2')]//a")
+  private List<ExtendedWebElement> brandLinks;
 
-    public HomePage(WebDriver driver) {
-        super(driver);
+  public HomePage(WebDriver driver) {
+    super(driver);
+  }
+
+  public FooterMenu getFooterMenu() {
+    return footerMenu;
+  }
+
+  public BrandModelsPage selectBrand(String brand) {
+    LOGGER.info("selecting '" + brand + "' brand...");
+    for (ExtendedWebElement brandLink : brandLinks) {
+      String currentBrand = brandLink.getText();
+      LOGGER.info("currentBrand: " + currentBrand);
+      if (brand.equalsIgnoreCase(currentBrand)) {
+        brandLink.click();
+        return new BrandModelsPage(driver);
+      }
     }
+    throw new RuntimeException("Unable to open brand: " + brand);
+  }
 
-    public FooterMenu getFooterMenu() {
-        return footerMenu;
-    }
-
-    public BrandModelsPage selectBrand(String brand) {
-        LOGGER.info("selecting '" + brand + "' brand...");
-        for (ExtendedWebElement brandLink : brandLinks) {
-            String currentBrand = brandLink.getText();
-            LOGGER.info("currentBrand: " + currentBrand);
-            if (brand.equalsIgnoreCase(currentBrand)) {
-                brandLink.click();
-                return new BrandModelsPage(driver);
-            }
-        }
-        throw new RuntimeException("Unable to open brand: " + brand);
-    }
-
-    public WeValuePrivacyAd getWeValuePrivacyAd() {
-    	return new WeValuePrivacyAd(driver);
-    }
+  public WeValuePrivacyAd getWeValuePrivacyAd() {
+    return new WeValuePrivacyAd(driver);
+  }
 }
